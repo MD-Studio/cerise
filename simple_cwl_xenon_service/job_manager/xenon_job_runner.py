784a69d4f8a510712060f71cf4899c4b45dd2263
Lourens Veen <l.veen@esciencecenter.nl>
2017-03-08 18:53:18 +0100

    Split off a XenonRemoteFiles class from XenonJobRunner; fixes #3

--- simple_cwl_xenon_service/job_manager/xenon_job_runner.py
+++ simple_cwl_xenon_service/job_manager/xenon_job_runner.py
@@ -3,6 +3,7 @@ import requests
 import xenon
 from xenon.files import OpenOption
 
+from .xenon_remote_files import XenonRemoteFiles
 from .job_state import JobState
 
 class XenonJobRunner:
@@ -20,13 +21,11 @@ class XenonJobRunner:
         """The Xenon instance to use."""
         self._sched = None
         """The Xenon scheduler to start jobs through."""
-        self._fs = None
-        """The Xenon remote file system to stage to."""
-        self._basedir = None
-        """The remote path to the base directory where we store our stuff."""
 
         self._init_xenon(xenon_config)
-        self._init_remote(xenon_config)
+
+        self._files = XenonRemoteFiles(self._x, xenon_config)
+        """The Xenon remote file system to stage to."""
 
     def _init_xenon(self, xenon_config):
         """Initialise Xenon, and set up Xenon objects to work with.
@@ -46,25 +45,6 @@ class XenonJobRunner:
         self._x = xenon.Xenon()
         # TODO: use config
         self._sched = self._x.jobs().newScheduler('local', None, None, None)
-        self._fs = self._x.files().newFileSystem('local', None, None, None)
-
-    def _init_remote(self, xenon_config):
-        """Set up a couple of dirs for running jobs, if they don't exist.
-
-        Args:
-            xenon_config: A dict containing key-value pairs with Xenon
-                configuration.
-        """
-        # TODO: use config
-        self._basedir = '/tmp/simple_cwl_xenon_service'
-        basedirpath = self._make_xenon_path('')
-        if not self._x.files().exists(basedirpath):
-            raise RuntimeError('Configuration error: Base directory not found on remote file system')
-
-        self._make_remote_dir('jobs', True)
-
-        # TODO: do a simple test run to check that cwl-runner works (?)
-
 
     def update(self, job_id):
         """Get status from Xenon and update store.
@@ -73,7 +53,7 @@ class XenonJobRunner:
             job_id: ID of the job to get the status of.
         """
         job = self._job_store.get_job(job_id)
-        log = self._read_remote_file('jobs/' + job_id + '/stderr.txt')
+        log = self._files.read_from_file(job_id, 'stderr.txt')
 
         if len(log) > 0:
             job.set_log(log.decode())
@@ -134,29 +114,25 @@ class XenonJobRunner:
         """
         job = self._job_store.get_job(job_id)
 
-        # create work dir
-        job_dir = 'jobs/' + job_id
-        job_workdir = job_dir + '/work'
-        self._make_remote_dir(job_workdir)
+        self._files.create_work_dir(job_id)
 
         # stage workflow
-        workflow_file_name = job_dir + '/workflow.cwl'
         if '://' in job.workflow:
             workflow_content = requests.get(job.workflow).content
         else:
             workflow_content = open(job.workflow, 'rb').read()
-        self._stage_file(workflow_file_name, workflow_content)
+        self._files.write_to_file(job_id, 'workflow.cwl', workflow_content)
 
         # stage name of the job
-        self._stage_file(job_dir + '/name.txt', job.get_name().encode('utf-8'))
+        self._files.write_to_file(job_id, 'name.txt', job.get_name().encode('utf-8'))
 
         # submit job
         xenon_jobdesc = xenon.jobs.JobDescription()
-        xenon_jobdesc.setWorkingDirectory(self._to_remote_path(job_workdir))
+        xenon_jobdesc.setWorkingDirectory(self._files.get_work_dir_path(job_id))
         xenon_jobdesc.setExecutable('cwl-runner')
-        xenon_jobdesc.setArguments([self._to_remote_path(workflow_file_name)])
-        xenon_jobdesc.setStdout(self._to_remote_path(job_dir + '/stdout.txt'))
-        xenon_jobdesc.setStderr(self._to_remote_path(job_dir + '/stderr.txt'))
+        xenon_jobdesc.setArguments([self._files.get_remote_file_path(job_id, 'workflow.cwl')])
+        xenon_jobdesc.setStdout(self._files.get_remote_file_path(job_id, '/stdout.txt'))
+        xenon_jobdesc.setStderr(self._files.get_remote_file_path(job_id, '/stderr.txt'))
         xenon_job = self._x.jobs().submitJob(self._sched, xenon_jobdesc)
         job.set_runner_data(xenon_job)
         job.set_state(JobState.WAITING)
@@ -171,76 +147,4 @@ class XenonJobRunner:
     def delete_job(self, job_id):
         job = self._job_store.get_job(job_id)
         self.cancel_job(job_id)
-        # delete files from server
-        job_dir = 'jobs/' + job_id
-        self._rm_remote_dir(job_dir, True)
-
-    def _run_remote(self, rel_workdir, command, args):
-        desc = xenon.jobs.JobDescription()
-        desc.setWorkingDirectory(self._to_remote_path(rel_workdir))
-        desc.setExecutable(command)
-        desc.setArguments(args)
-        xenon_job = self._x.jobs().submitJob(self._sched, desc)
-        # Note: setting to 0 for an infinite timeout doesn't work
-        self._x.jobs().waitUntilDone(xenon_job, 100000)
-
-    def _make_remote_dir(self, rel_path, existing_ok=False):
-        try:
-            xenonpath = self._make_xenon_path(rel_path)
-            self._x.files().createDirectories(xenonpath)
-        except jpype.JException(xenon.nl.esciencecenter.xenon.files.PathAlreadyExistsException):
-            if not existing_ok:
-                raise
-            else:
-                pass
-
-    def _rm_remote_dir(self, rel_path, recursive):
-        x_remote_path = self._make_xenon_path(rel_path)
-        if recursive:
-            self._x_recursive_delete(x_remote_path)
-        else:
-            self._x.files().delete(x_remote_path)
-
-    def _x_recursive_delete(self, x_remote_path):
-        x_dir = self._x.files().newAttributesDirectoryStream(x_remote_path)
-        x_dir_it = x_dir.iterator()
-        while x_dir_it.hasNext():
-            x_path_attr = x_dir_it.next()
-            if x_path_attr.attributes().isDirectory():
-                self._x_recursive_delete(x_path_attr.path())
-            else:
-                self._x.files().delete(x_path_attr.path())
-        self._x.files().delete(x_remote_path)
-
-    def _stage_file(self, rel_path, data):
-        """Write a file on the remote resource containing the given raw data.
-        Args:
-            rel_path A string containing a relative remote path
-            data A bytes-type object containing the data to write
-        """
-        x_remote_path = self._make_xenon_path(rel_path)
-        stream = self._x.files().newOutputStream(x_remote_path, [OpenOption.CREATE, OpenOption.TRUNCATE])
-        stream.write(data)
-        stream.close()
-
-    def _read_remote_file(self, rel_path):
-        result = bytearray()
-
-        x_remote_path = self._make_xenon_path(rel_path)
-        if self._x.files().exists(x_remote_path):
-            stream = self._x.files().newInputStream(x_remote_path)
-            buf = jpype.JArray(jpype.JByte)(1024)
-            bytes_read = stream.read(buf)
-            while bytes_read != -1:
-                result = bytearray().join([result, bytearray(buf[0:bytes_read])])
-                bytes_read = stream.read(buf)
-
-        return result
-
-    def _to_remote_path(self, rel_path):
-        return self._basedir + '/' + rel_path
-
-    def _make_xenon_path(self, rel_path):
-        remote_path = self._to_remote_path(rel_path)
-        xenon_path = xenon.files.RelativePath(remote_path)
-        return self._x.files().newPath(self._fs, xenon_path)
+        self._files.remove_work_dir(job_id)
---
+++ simple_cwl_xenon_service/job_manager/xenon_remote_files.py
@@ -0,0 +1,145 @@
+import jpype
+import xenon
+from xenon.files import OpenOption
+
+from .job_state import JobState
+
+class XenonRemoteFiles:
+    def __init__(self, xenon, xenon_config={}):
+        """Create a XenonRemoteFiles object.
+        Sets up remote directory structure as well, but refuses to
+        create the top-level directory.
+
+        Args:
+            xenon: The Xenon object to use.
+            xenon_config: A dict containing key-value pairs with Xenon
+                configuration.
+        """
+        self._x = xenon
+        """The Xenon instance to use."""
+        # TODO: use config
+        self._fs = self._x.files().newFileSystem('local', None, None, None)
+        """The Xenon remote file system to stage to."""
+        self._basedir = '/tmp/simple_cwl_xenon_service'
+        """The remote path to the base directory where we store our stuff."""
+
+        # TODO: use config
+        basedirpath = self._make_xenon_path('')
+        if not self._x.files().exists(basedirpath):
+            raise RuntimeError('Configuration error: Base directory not found on remote file system')
+
+        self._make_remote_dir('jobs', True)
+
+    def create_work_dir(self, job_id):
+        """Create a work directory for a job.
+
+        Args:
+            job_id: The id of the job to make a work directory for.
+        """
+        self._make_remote_dir('jobs/' + job_id + '/work')
+
+    def get_work_dir_path(self, job_id):
+        """Return the job's work directory as an absolute path on the compute resource.
+
+        Args:
+            job_id: The id of the job whose work directory to return
+        """
+        return self._to_remote_path('jobs/' + job_id + '/work')
+
+    def remove_work_dir(self, job_id):
+        """Remove the work directory for a job.
+        This will remove the directory and everything in it.
+
+        Args:
+            job_id: The id of the job whose work directory to delete.
+        """
+        self._rm_remote_dir('jobs/' + job_id, True)
+
+    def get_remote_file_path(self, job_id, rel_path):
+        """Get a corresponding absolute path on the compute resource.
+
+        Args:
+            job_id: The of the job that is the context for this path
+            rel_path: A string with a path relative to the job's directory
+        """
+        return self._to_remote_path('jobs/' + job_id + '/' + rel_path)
+
+    def write_to_file(self, job_id, rel_path, data):
+        """Write the data to a remote file.
+
+        Args:
+            job_id: The id of the job to write data for
+            rel_path: A string with a path relative to the job's directory
+            data: A bytes-object containing the data to write
+        """
+        self._write_remote_file('jobs/' + job_id + '/' + rel_path, data)
+
+    def read_from_file(self, job_id, rel_path):
+        """Read data from a remote file.
+
+        Args:
+            job_id: A job from whose work dir a file is read
+            rel_path: A string with a path relative to the job's directory
+        """
+        return self._read_remote_file('jobs/' + job_id + '/' + rel_path)
+
+    def _make_remote_dir(self, rel_path, existing_ok=False):
+        try:
+            xenonpath = self._make_xenon_path(rel_path)
+            self._x.files().createDirectories(xenonpath)
+        except jpype.JException(xenon.nl.esciencecenter.xenon.files.PathAlreadyExistsException):
+            if not existing_ok:
+                raise
+            else:
+                pass
+
+    def _rm_remote_dir(self, rel_path, recursive):
+        x_remote_path = self._make_xenon_path(rel_path)
+        if recursive:
+            self._x_recursive_delete(x_remote_path)
+        else:
+            self._x.files().delete(x_remote_path)
+
+    def _x_recursive_delete(self, x_remote_path):
+        x_dir = self._x.files().newAttributesDirectoryStream(x_remote_path)
+        x_dir_it = x_dir.iterator()
+        while x_dir_it.hasNext():
+            x_path_attr = x_dir_it.next()
+            if x_path_attr.attributes().isDirectory():
+                self._x_recursive_delete(x_path_attr.path())
+            else:
+                self._x.files().delete(x_path_attr.path())
+        self._x.files().delete(x_remote_path)
+
+    def _write_remote_file(self, rel_path, data):
+        """Write a file on the remote resource containing the given raw data.
+        Args:
+            rel_path A string containing a relative remote path
+            data A bytes-type object containing the data to write
+        """
+        x_remote_path = self._make_xenon_path(rel_path)
+        stream = self._x.files().newOutputStream(x_remote_path, [OpenOption.CREATE, OpenOption.TRUNCATE])
+        stream.write(data)
+        stream.close()
+
+    def _read_remote_file(self, rel_path):
+        result = bytearray()
+
+        x_remote_path = self._make_xenon_path(rel_path)
+        if self._x.files().exists(x_remote_path):
+            stream = self._x.files().newInputStream(x_remote_path)
+            buf = jpype.JArray(jpype.JByte)(1024)
+            bytes_read = stream.read(buf)
+            while bytes_read != -1:
+                result = bytearray().join([result, bytearray(buf[0:bytes_read])])
+                bytes_read = stream.read(buf)
+
+        return result
+
+    def _to_remote_path(self, rel_path):
+        return self._basedir + '/' + rel_path
+
+    def _make_xenon_path(self, rel_path):
+        remote_path = self._to_remote_path(rel_path)
+        xenon_path = xenon.files.RelativePath(remote_path)
+        return self._x.files().newPath(self._fs, xenon_path)
