fb386da0da6a78a1f9c046ecc998b80268041ac1
Lourens Veen <l.veen@esciencecenter.nl>
2018-12-31 07:02:51 +0100

    Clean up test_remote_api

--- cerise/back_end/test/test_remote_api.py
+++ cerise/back_end/test/test_remote_api.py
@@ -4,55 +4,52 @@ from cerise.back_end.remote_api import RemoteApi
 import cerulean
 
 import os
+from pathlib import Path
 import pytest
 
 
 @pytest.fixture
-def installation(tmpdir, mock_config):
-    local_api_dir = os.path.join(os.path.dirname(__file__), 'api')
-    remote_api_files = RemoteApi(mock_config, local_api_dir)
-    remote_api_files.install()
-    return mock_config.get_basedir()
+def installed_api_dir(mock_config, local_api_dir):
+    remote_api = RemoteApi(mock_config, str(local_api_dir))
+    remote_api.install()
+    return mock_config.get_basedir() / 'api'
 
 
-def test_install(installation):
+def test_install(installed_api_dir):
     # check that steps were staged
-    assert (installation / 'api' / 'test' / 'steps' / 'test' /
-            'wc.cwl').is_file()
+    test_steps_dir = installed_api_dir / 'test' / 'steps'
+    assert (test_steps_dir / 'test' / 'wc.cwl').is_file()
 
     # check that install script was run
-    assert (installation / 'api' / 'test' / 'files' / 'test' /
-            'test_file.txt').is_file()
+    test_files_dir = installed_api_dir / 'test' / 'files'
+    assert (test_files_dir / 'test' / 'test_file.txt').is_file()
 
     # check that it was run once
-    assert ((installation / 'api' / 'count.txt'
-            ).read_text('utf-8').strip() == '1')
+    assert (installed_api_dir / 'count.txt').read_text().strip() == '1'
 
 
-def test_update(installation, mock_config):
-    local_api_dir = os.path.join(os.path.dirname(__file__), 'api_new')
-    remote_api_files = RemoteApi(mock_config, local_api_dir)
+def test_update(installed_api_dir, mock_config):
+    local_api_dir = Path(__file__).parent / 'api_new'
+    remote_api_files = RemoteApi(mock_config, str(local_api_dir))
 
     assert remote_api_files.update_available()
     remote_api_files.install()
     assert not remote_api_files.update_available()
 
-    assert not (installation / 'api' / 'test' / 'files' / 'test' /
-            'test_file.txt').exists()
-    assert (installation / 'api' / 'test' / 'files' / 'test' /
-            'test_file2.txt').exists()
+    test_files_dir = installed_api_dir / 'test' / 'files'
+    assert not (test_files_dir / 'test' / 'test_file.txt').exists()
+    assert (test_files_dir / 'test' / 'test_file2.txt').exists()
 
 
-def test_dev_update(installation, mock_config):
-    local_api_dir = os.path.join(os.path.dirname(__file__), 'api')
-    remote_api_files = RemoteApi(mock_config, local_api_dir)
+def test_dev_update(installed_api_dir, mock_config):
+    local_api_dir = Path(__file__).parent / 'api'
+    remote_api_files = RemoteApi(mock_config, str(local_api_dir))
 
     assert remote_api_files.update_available()
     remote_api_files.install()
 
     # check that it was run a second time
-    assert ((installation / 'api' / 'count.txt'
-            ).read_text('utf-8').strip() == '2')
+    assert (installed_api_dir / 'count.txt').read_text().strip() == '2'
 
     # check that it will keep reinstalling
     assert remote_api_files.update_available()
